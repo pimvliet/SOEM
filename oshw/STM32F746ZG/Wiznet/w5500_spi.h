2f3d5b062e02aa1a5383e08739e5b95f9e08379b
Pim van der Vliet <pim@edingcnc.com>
2022-12-06 13:17:27 +0100

    Add cpp support

--- oshw/STM32F746ZG/Wiznet/w5500_spi.h
+++ oshw/STM32F746ZG/Wiznet/w5500_spi.h
@@ -1,9 +1,18 @@
 #ifndef W5500_SPI_H
 #define W5500_SPI_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include "stdio.h"
 #include "stm32f7xx.h"
 
 int W5500Init(GPIO_TypeDef* csPort, uint16_t csPin, GPIO_TypeDef* rstPort, uint16_t rstPin);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
